31297f446267dc6789f58463ca915ea3f59409a5
GastonUC <Gacox.magallanes@gmail.com>
2020-09-10 12:51:10 -0300

    Displaying with page.html but on html form

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+from markdown2 import Markdown
 
 
 def index(request):
@@ -8,3 +9,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, entry):
+    entryPage = util.get_entry(entry)
+    if entryPage is None:
+        return render(request, "encyclopedia/error.html", {
+            "entryTitle": entry
+        })
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "entry": Markdown().convert(entryPage),
+            "entryTitle": entry
+        })
